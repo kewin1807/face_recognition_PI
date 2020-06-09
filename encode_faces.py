bd833f8fcfc627cf620783a535f26718047da903
BatmanBruceWayne <taideptrai02@gmail.com>
2020-06-09 17:16:44 +0700

    update a little

--- encode_faces.py
+++ encode_faces.py
@@ -14,9 +14,9 @@ import os
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--dataset", required=True,
+ap.add_argument("-i", "--dataset", type=str, default='./dataset',
 	help="path to input directory of faces + images")
-ap.add_argument("-e", "--encodings", required=True,
+ap.add_argument("-e", "--encodings", type=str, default='encodings.pickle',
 	help="path to serialized db of facial encodings")
 ap.add_argument("-d", "--detection-method", type=str, default="cnn",
 	help="face detection model to use: either `hog` or `cnn`")
@@ -34,8 +34,7 @@ knownNames = []
 for (i, imagePath) in enumerate(imagePaths):
 	# extract the person name from the image path
 	print(imagePath)
-	print("[INFO] processing image {}/{}".format(i + 1,
-		len(imagePaths)))
+	print("[INFO] processing image {}/{}".format(i + 1, len(imagePaths)))
 	name = imagePath.split(os.path.sep)[-2]
 
 	# load the input image and convert it from RGB (OpenCV ordering)
@@ -45,8 +44,7 @@ for (i, imagePath) in enumerate(imagePaths):
 
 	# detect the (x, y)-coordinates of the bounding boxes
 	# corresponding to each face in the input image
-	boxes = face_recognition.face_locations(rgb,
-		model=args["detection_method"])
+	boxes = face_recognition.face_locations(rgb, model=args["detection_method"])
 
 	# compute the facial embedding for the face
 	encodings = face_recognition.face_encodings(rgb, boxes)
